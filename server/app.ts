1be2fa8bcb4b1d3a3114d537f1f073bc73af4e58
Kirti Krishnn <kirti-krishnn@users.noreply.github.com>
2026-09-18 09:37:59 +0530

    Configure trust proxy for Vercel

--- server/app.ts
+++ server/app.ts
@@ -18,6 +18,8 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
+
+app.set('trust proxy', 1);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const serverRoot = path.basename(__dirname) === 'dist' ? path.resolve(__dirname, '..') : __dirname;
