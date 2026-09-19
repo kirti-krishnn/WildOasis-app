23425db19cafbf868a4859d7758a7bde945f63c3
Kirti Krishnn <kirti-krishnn@users.noreply.github.com>
2026-09-19 22:47:20 +0530

    Use shared auth secret in NextAuth middleware

--- customer/middleware.js
+++ customer/middleware.js
@@ -1,11 +1,14 @@
 import { withAuth } from "next-auth/middleware";
 
 export const middleware = withAuth({
+  secret: process.env.AUTH_SECRET,
+
   callbacks: {
     authorized({ token }) {
       return !!token;
     },
   },
+
   pages: {
     signIn: "/login",
   },
